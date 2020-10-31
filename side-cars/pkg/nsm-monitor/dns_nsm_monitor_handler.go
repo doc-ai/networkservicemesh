5911ae32afc58f07ef71ed12baa9ca15c0f1136d
veni <veni@doc.ai>
2020-10-30 23:20:56 -0700

    enhancing nsm-dns-monitor sidecar to update Corefile with host records for the service endpoint peers

--- side-cars/pkg/nsm-monitor/dns_nsm_monitor_handler.go
+++ side-cars/pkg/nsm-monitor/dns_nsm_monitor_handler.go
@@ -1,6 +1,9 @@
 package nsmmonitor
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/networkservicemesh/networkservicemesh/utils"
 	"github.com/networkservicemesh/networkservicemesh/utils/caddyfile"
 	"github.com/networkservicemesh/networkservicemesh/utils/dnsconfig"
@@ -8,7 +11,16 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/networkservicemesh/networkservicemesh/controlplane/api/connection"
+	caddyfile_utils "github.com/networkservicemesh/networkservicemesh/utils/caddyfile"
 )
+
+const dnsPeersAsHostsEnv = "DNS_PEERS_AS_HOSTS"
+
+type nsmConnPeer struct {
+	serviceName    string
+	serviceDomain  string
+	serviceDstAddr string
+}
 
 //nsmDNSMonitorHandler implements Handler interface for handling dnsConfigs
 type nsmDNSMonitorHandler struct {
@@ -16,14 +28,8 @@ type nsmDNSMonitorHandler struct {
 	manager  dnsconfig.Manager
 	reloadOp utils.Operation
 	path     string
-}
 
-func (m *nsmDNSMonitorHandler) Updated(old, new *connection.Connection) {
-	logrus.Infof("Deleting config with id %v", old.Id)
-	m.manager.Delete(old.Id)
-	logrus.Infof("Adding config with id %v", new.Id)
-	m.manager.Store(new.Id, new.GetContext().GetDnsContext().GetConfigs()...)
-	m.reloadOp.Run()
+	nsmConnPeers map[string]nsmConnPeer
 }
 
 //NewNsmDNSMonitorHandler creates new DNS monitor handler
@@ -34,11 +40,25 @@ func NewNsmDNSMonitorHandler() Handler {
 		logrus.Fatalf("An error during parse corefile: %v", err)
 	}
 	m := &nsmDNSMonitorHandler{
-		manager: mgr,
-		path:    p,
+		manager:      mgr,
+		path:         p,
+		nsmConnPeers: map[string]nsmConnPeer{},
 	}
+
+	dnsPeersAsHosts := utils.EnvVar(dnsPeersAsHostsEnv).GetBooleanOrDefault(false)
 	m.reloadOp = utils.NewSingleAsyncOperation(func() {
-		err := m.manager.Caddyfile(m.path).Save()
+		c := m.manager.Caddyfile(m.path)
+
+		if dnsPeersAsHosts {
+			logrus.Debugf("writing nsm connection peers to Corefile: %v", m.nsmConnPeers)
+			for _, p := range m.nsmConnPeers {
+				updatePeerDnsHosts(c, p)
+			}
+			logrus.Debugf("updated Corefile: %v", c.String())
+		}
+
+		err := c.Save()
+
 		if err != nil {
 			logrus.Error(err)
 		}
@@ -46,14 +66,76 @@ func NewNsmDNSMonitorHandler() Handler {
 	return m
 }
 
+func updatePeerDnsHosts(c caddyfile_utils.Caddyfile, peer nsmConnPeer) {
+	updatePeerDnsHostsHelper(c, peer, false)
+	updatePeerDnsHostsHelper(c, peer, true)
+}
+
+func updatePeerDnsHostsHelper(c caddyfile_utils.Caddyfile, peer nsmConnPeer, addSuffix bool) {
+
+	connPeerName := peer.serviceName
+	if addSuffix {
+		connPeerName = fmt.Sprintf("%s.nsm", peer.serviceName)
+	}
+	connPeerAddr := strings.Split(peer.serviceDstAddr, "/")[0]
+
+	var peerScope caddyfile_utils.Scope = nil
+	if !c.HasScope(connPeerName) {
+		peerScope = c.WriteScope(connPeerName).Write("reload 2s").Write("log")
+	} else {
+		peerScope = c.GetOrCreate(connPeerName)
+	}
+
+	var hostsScope caddyfile_utils.Scope = nil
+	if !peerScope.HasScope("hosts") {
+		hostsScope = peerScope.WriteScope("hosts")
+		//hostsScope.Write("no_recursive")
+		//hostsScope.Write("fallthrough")
+	} else {
+		hostsScope = peerScope.GetOrCreate("hosts")
+	}
+
+	record := fmt.Sprintf("%s   %s", connPeerAddr, connPeerName)
+	hostsScope.Write(record)
+}
+
+func (m *nsmDNSMonitorHandler) Updated(old, new *connection.Connection) {
+	logrus.Infof("Deleting config with id %v", old.Id)
+	m.manager.Delete(old.Id)
+	logrus.Infof("Adding config with id %v", new.Id)
+	m.manager.Store(new.Id, new.GetContext().GetDnsContext().GetConfigs()...)
+
+	netSvcName, nsmAddress := parseNetSvc(new.NetworkService)
+
+	delete(m.nsmConnPeers, old.Id)
+	peer := nsmConnPeer{
+		//serviceName:    new.NetworkService,
+		serviceName:    netSvcName,
+		serviceDomain:  nsmAddress,
+		serviceDstAddr: new.Context.IpContext.DstIpAddr,
+	}
+	logrus.Debugf("Updating nsm connection peer with %v: %v", new.Id, peer)
+	m.nsmConnPeers[new.Id] = peer
+
+	m.reloadOp.Run()
+}
+
 func (m *nsmDNSMonitorHandler) Connected(conns map[string]*connection.Connection) {
 	for _, conn := range conns {
 		logrus.Info(conn.Context.DnsContext)
-		err := m.manager.Caddyfile(m.path).Save()
-		if err != nil {
-			logrus.Error(err)
-		}
+		m.reloadOp.Run()
 		m.manager.Store(conn.Id, conn.GetContext().GetDnsContext().GetConfigs()...)
+
+		netSvcName, nsmAddress := parseNetSvc(conn.NetworkService)
+
+		peer := nsmConnPeer{
+			//serviceName:    conn.NetworkService,
+			serviceName:    netSvcName,
+			serviceDomain:  nsmAddress,
+			serviceDstAddr: conn.Context.IpContext.DstIpAddr,
+		}
+		logrus.Debugf("Adding nsm connection peer with %v: %v", conn.Id, peer)
+		m.nsmConnPeers[conn.Id] = peer
 	}
 	m.reloadOp.Run()
 }
@@ -61,5 +143,18 @@ func (m *nsmDNSMonitorHandler) Connected(conns map[string]*connection.Connection
 func (m *nsmDNSMonitorHandler) Closed(conn *connection.Connection) {
 	logrus.Infof("Deleting config with id %v", conn.Id)
 	m.manager.Delete(conn.Id)
+
+	logrus.Debugf("Deleting nsm connection peer with %v", conn.Id)
+	delete(m.nsmConnPeers, conn.Id)
+
 	m.reloadOp.Run()
+}
+
+func parseNetSvc(netSvcUrl string) (netSvcName string, nsmAddress string) {
+	if !strings.Contains(netSvcUrl, "@") {
+		return netSvcUrl, ""
+	}
+
+	t := strings.SplitN(netSvcUrl, "@", 2)
+	return t[0], t[1]
 }
